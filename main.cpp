9dea579f3baad556c7dc063f4e457d8eb26a40f1
MADMAN-Modding <mad@kmdtech.net>
2024-07-08 22:29:59 -0400

    converting to 6 bits then 8 bits works with part of the 8 bit to int working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <map>
 #include <bitset>
+#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 class Main {
@@ -31,13 +33,62 @@ class Main {
             // Map '/' to 63
             base64CharMap['/'] = 63;
 
-                // Output the map for verification
-            for (const auto& pair : base64CharMap) {
-                std::cout << pair.first << " : " << pair.second << std::endl;
-            }
+            // Output the map for verification
+            // for (const auto& pair : base64CharMap) {
+            //     std::cout << pair.first << " : " << pair.second << std::endl;
+            // }
         }
+
+        string encode(string input) {
+            string base64;
+            vector <int> base8;
+
+            // Keeps track of the currently watched 8 bits
+            string base8Tracking;
+
+            for (int i = 0; i < input.length(); i++)
+            {
+                base64 += bitset<6>(base64CharMap[input[i]]).to_string();
+            }
+
+
+
+            for (int i = 1; i <= base64.length(); i++) {
+                // cout << base64[i];
+
+                // base64 = i % 8 == 0? base64.replace(i, 8, "") : base64;
+                base8Tracking += base64[i];
+
+                if (i % 8 == 0) {
+                    // cout << "\n";
+                    // Add the current set of 8 bits and then convert them to integers
+                    base8.push_back(stoi(base8Tracking));
+                    base8Tracking = "";
+                }
+
+                // cout << "New: " << base64;
+            }
+            
+            for (int i = 0; i < base8.size(); i++) {
+
+                cout << bitset<8>(base8[i]).to_ulong() << "\n";
+
+            }
+            
+
+            return base64;
+        };
 };
 
 int main() {
     Main main;
+
+    string input;
+
+    cout << "Input a string please: ";
+
+    cin >> input;
+
+    main.encode(input);
+
 }

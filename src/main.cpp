0e1cc6faa0c69861c1f18f067f92a5da1b4f6d6b
MADMAN-Modding <mad@kmdtech.net>
2024-07-11 18:21:45 -0400

    bug fixing and comments

--- src/main.cpp
+++ src/main.cpp
@@ -31,22 +31,48 @@ int main()
             std::string command = event.command.get_command_name();
                 
             if (command == "encode") {
+                // Gets the input
                 std::string text = std::get<std::string>(event.get_parameter("input"));
 
+                // If the user doesn't want the original text shown then it will set the extra variable to ""
                 std::string extra = std::get<bool>(event.get_parameter("show_original")) ? text + " in base 64 is: " : "";
                 
-                int ephemeral = std::get<bool>(event.get_parameter("hidden")) ? dpp::m_ephemeral : dpp::m_put;
+                // Initializes the ephemeral variable to put
+                int ephemeral = dpp::m_put;
 
+                // This allows the code to not crash when the variable isn't set
+                try
+                {                    
+                    // If the user wants the text hidden then it will be ephemeral, else put
+                    ephemeral = std::get<bool>(event.get_parameter("hidden")) ? dpp::m_ephemeral : ephemeral;
+                }
+                catch(const std::exception& e) {}
+                
+                // Sends the message to the user
                 event.reply(dpp::message(extra + encode(text)).set_flags(ephemeral));
+
             } else if (command == "decode") {
+                // Gets the input
                 std::string text = std::get<std::string>(event.get_parameter("input"));
                 
+                // If the user doesn't want the original text shown then it will set the extra variable to ""
                 std::string extra = std::get<bool>(event.get_parameter("show_original")) ? text + " from base 64 is: " : "";
 
-                int ephemeral = std::get<bool>(event.get_parameter("hidden")) ? dpp::m_ephemeral : dpp::m_put;
+                // Initializes the ephemeral variable to put
+                int ephemeral = dpp::m_put;
 
+                // This allows the code to not crash when the variable isn't set
+                try
+                {                    
+                    // If the user wants the text hidden then it will be ephemeral, else put
+                    ephemeral = std::get<bool>(event.get_parameter("hidden")) ? dpp::m_ephemeral : ephemeral;
+                }
+                catch(const std::exception& e) {}
+
+                // Sends the message to the user
                 event.reply(dpp::message(extra + decode(text)).set_flags(ephemeral));          
             } else {
+                // If something messes up then send this
                 event.reply("Something went wrong");
             }
         });
@@ -55,18 +81,18 @@ int main()
                  {
                      if (dpp::run_once<struct register_bot_commands>())
                      {
-                        // Wipe current commands
-                        // bot.global_bulk_command_delete();
-
+                        // Makes the commands
                         dpp::slashcommand encode("encode", "Enter text to encode", bot.me.id);
                         dpp::slashcommand decode("decode", "Enter text to decode", bot.me.id);
 
+                        // Encoding commands
                         encode.add_option(
                             dpp::command_option(dpp::co_string, "input", "The text to encode", true));
                         encode.add_option(dpp::command_option(dpp::co_boolean, "show_original", "Show the original text?", true));
 
                         encode.add_option(dpp::command_option(dpp::co_boolean, "hidden", "Keep the response hidden to you?", false)); 
                         
+                        // Decoding commands
                         decode.add_option(
                             dpp::command_option(dpp::co_string, "input", "The text to decode", true));
                         decode.add_option(
@@ -74,6 +100,7 @@ int main()
 
                         decode.add_option(dpp::command_option(dpp::co_boolean, "hidden", "Keep the response hidden to you?", false)); 
 
+                        // Creates the commands 
                         bot.global_command_create(encode);
                         bot.global_command_create(decode);
                      } });

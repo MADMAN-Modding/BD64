ed420ad1ed55af22950f706b858830d8065b844b
MADMAN-Modding <mad@kmdtech.net>
2024-07-11 17:44:54 -0400

    decoding works

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,13 @@ std::string encode(std::string text)
     return encode.encode(text);
 };
 
+std::string decode(std::string text)
+{
+    Decode decode;
+
+    return decode.decode(text);
+}
+
 int main()
 {
     dpp::cluster bot(BOT_TOKEN);
@@ -20,30 +27,44 @@ int main()
     bot.on_log(dpp::utility::cout_logger());
 
     bot.on_slashcommand([](const dpp::slashcommand_t &event)
-                        {
-        if (event.command.get_command_name() == "encode") {
-            std::string text = std::get<std::string>(event.get_parameter("input"));
-            
-            // event.reply(std::string(encode(text)));
-            event.reply(text + " in base64 is: " + encode(text));
-        } else {
-            event.reply("Not right here");          
-        } });
+        {
+            std::string command = event.command.get_command_name();
+                
+            if (command == "encode") {
+                std::string text = std::get<std::string>(event.get_parameter("input"));
+
+                
+                
+                event.reply(text + " in base64 is: " + encode(text));
+            } else if (command == "decode") {
+                std::string text = std::get<std::string>(event.get_parameter("input"));
+                
+                event.reply(text + " from base64 is: " + decode(text));          
+            } else {
+                event.reply("Something went wrong");
+            }
+        });
 
     bot.on_ready([&bot](const dpp::ready_t &event)
                  {
                      if (dpp::run_once<struct register_bot_commands>())
                      {
-                         //     bot.global_command_create(dpp::slashcommand("encode", "Input a string to encode", bot.me.id)
-                         //     .add_option(dpp::command_option(dpp::co_string, "text", "What you want to encode")));
-                         dpp::slashcommand encode("encode", "Enter text to encode", bot.me.id);
+                        // // Wipe current commands
+                        // bot.global_bulk_command_delete();
 
-                         encode.add_option(
-                             dpp::command_option(dpp::co_string, "input", "The text to encode", true));
+                        dpp::slashcommand encode("encode", "Enter text to encode", bot.me.id);
+                        dpp::slashcommand decode("decode", "Enter text to decode", bot.me.id);
+
+                        encode.add_option(
+                            dpp::command_option(dpp::co_string, "input", "The text to encode", true));
+                        
+                        decode.add_option(
+                            dpp::command_option(dpp::co_string, "input", "The text to decode", true));
+
 
                         bot.global_command_create(encode);
-                     }
-                 });
+                        bot.global_command_create(decode);
+                     } });
 
     bot.start(dpp::st_wait);
 
